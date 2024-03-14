83023da732926be280c2362e7582cf9453de0cef
Samantha Kosman <skosman@uvic.ca>
2024-03-14 16:51:40 -0600

    Remove unnecessary files related to vue setup and add FlowBite
    Removed any files that were related to the initial project set up to clean up components.
    In addition, add FlowBite to be used as an component library with the first element being a navigation bar.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.js"
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
 
