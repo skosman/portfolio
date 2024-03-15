7351bb45cbafe1006a980064e85c94d86765cb8f
Samantha Kosman <skosman@uvic.ca>
2024-03-15 11:38:00 -0600

    Add new pages for site and update navigation for them
    Added the pages that will contain the relevant information for the portfolio.
    Along with updating the router and navbar to properly route to those pages

--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,21 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import("../views/AboutView.vue"),
     },
+    {
+      path: "/experience",
+      name: "experience",
+      component: () => import("../views/ExperienceView.vue"),
+    },
+    {
+      path: "/projects",
+      name: "projects",
+      component: () => import("../views/ProjectsView.vue"),
+    },
+    {
+      path: "/contact",
+      name: "contact",
+      component: () => import("../views/ContactView.vue"),
+    },
   ],
 });
 
