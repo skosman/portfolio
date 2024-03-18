02f2e08118d63c6ab1a1b9c9c2cb760f7672799d
Samantha Kosman <skosman@uvic.ca>
2024-03-18 16:04:48 -0600

    Add more information to about me page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,25 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <section class="bg-white dark:bg-gray-900">
+    <div class="py-8 px-4 mx-auto max-w-screen-xl text-left lg:py-16">
+      <h1
+        class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white"
+      >
+        About Me
+      </h1>
+      <p class="text-lg font-normal text-gray-500 dark:text-gray-400 mb-6">
+        I am a software engineer on the hunt for a new position. I graduated
+        from the University of Victoria in August 2022 with a Bachelor of
+        Software Engineering with Co-op Designation. I have over 2 years of work
+        experience through my various co-ops and full-time position at Article.
+      </p>
+      <p class="text-lg font-normal text-gray-500 dark:text-gray-400 mb-6">
+        Outside of work I love to travel and explore different cultures. And
+        during the summer can find me in the mountains, hiking, camping and
+        trail running. You ever need a recommendation for trail in Alberta, hit
+        me up!
+      </p>
+    </div>
+  </section>
 </template>
 
 <style></style>
