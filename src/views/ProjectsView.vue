37df4757b8d20bceb1d2d90384cd207d085f35d2
Samantha Kosman <skosman@uvic.ca>
2024-03-20 16:28:32 -0600

    Add nicer headings to pages

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,7 +1,13 @@
 <template>
-  <div class="about">
-    <h1>This is a projects page</h1>
-  </div>
+  <section class="bg-white dark:bg-gray-900">
+    <div class="py-8 px-4 mx-auto max-w-screen-xl text-left lg:py-16">
+      <h1
+        class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white"
+      >
+        Projects
+      </h1>
+    </div>
+  </section>
 </template>
 
 <style></style>
