64d38a151f11eee129b6864e2486396bffc0f3a8
Samantha Kosman <skosman@uvic.ca>
2024-03-20 16:28:15 -0600

    Add footer to website with links to github and linkedin

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
 import NavBar from "./components/NavBar.vue";
+import Footer from "./components/Footer.vue";
 </script>
 
 <template>
   <NavBar></NavBar>
   <RouterView />
+  <Footer></Footer>
 </template>
 
 <style scoped></style>
