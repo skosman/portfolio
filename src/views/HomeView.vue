0f28b2f7499ce674d486ff53cd307bc84ec3cd2e
Sam Kosman <samkosman@Sams-MacBook-Pro.local>
2024-05-14 16:19:15 -0600

    Update cards with content that is relevant on home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -17,10 +17,10 @@
       <div
         class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0"
       >
-        <a
-          href="#"
-          class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
-        >
+      <RouterLink
+              to="/experience"
+              class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
+              >
           My Experience
           <svg
             class="w-3.5 h-3.5 ms-2 rtl:rotate-180"
@@ -37,13 +37,8 @@
               d="M1 5h12m0 0L9 1m4 4L9 9"
             />
           </svg>
-        </a>
-        <a
-          href="#"
-          class="py-3 px-5 sm:ms-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-70"
-        >
-          Learn more
-        </a>
+          </RouterLink>
+
       </div>
     </div>
   </section>
@@ -52,38 +47,40 @@
       <div
         class="bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12 mb-8"
       >
-        <a
-          href="#"
-          class="bg-blue-100 text-blue-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded-md dark:bg-gray-700 dark:text-blue-400 mb-2"
-        >
-          <svg
-            class="w-2.5 h-2.5 me-1.5"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            viewBox="0 0 20 14"
+      <a
+            href="#"
+            class="bg-purple-100 text-purple-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded-md dark:bg-gray-700 dark:text-purple-400 mb-2"
           >
-            <path
-              d="M11 0H2a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h9a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm8.585 1.189a.994.994 0 0 0-.9-.138l-2.965.983a1 1 0 0 0-.685.949v8a1 1 0 0 0 .675.946l2.965 1.02a1.013 1.013 0 0 0 1.032-.242A1 1 0 0 0 20 12V2a1 1 0 0 0-.415-.811Z"
-            />
-          </svg>
-          Tutorial
-        </a>
+            <svg
+              class="w-2.5 h-2.5 me-1.5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 20 16"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M5 4 1 8l4 4m10-8 4 4-4 4M11 1 9 15"
+              />
+            </svg>
+            Code
+          </a>
         <h1
           class="text-gray-900 dark:text-white text-3xl md:text-5xl font-extrabold mb-2"
         >
-          How to quickly deploy a static website
+          Full Stack Experience
         </h1>
         <p class="text-lg font-normal text-gray-500 dark:text-gray-400 mb-6">
-          Static websites are now used to bootstrap lots of websites and are
-          becoming the basis for a variety of tools that even influence both web
-          designers and developers.
+          I've worked in full-stack development, ranging from Java Spring Applications with Vue to using Python Django and React. I've been involved with the whole software development process, from testing with JUnit, Mockito, Selenium and PyUnit to setting up and deploying applications with CI/CD workflows on GitHub and Bitbucket to deploy on AWS. 
         </p>
         <a
           href="#"
           class="inline-flex justify-center items-center py-2.5 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
         >
-          Read more
+          Learn More
           <svg
             class="w-3.5 h-3.5 ms-2 rtl:rotate-180"
             aria-hidden="true"
@@ -125,12 +122,10 @@
           <h2
             class="text-gray-900 dark:text-white text-3xl font-extrabold mb-2"
           >
-            Start with Flowbite Design System
+            Agile Workflow
           </h2>
           <p class="text-lg font-normal text-gray-500 dark:text-gray-400 mb-4">
-            Static websites are now used to bootstrap lots of websites and are
-            becoming the basis for a variety of tools that even influence both
-            web designers and developers.
+            I love working in teams to develop great products. By communicating with stakeholders and end-users, I aim to deliver software that empowers users.  
           </p>
           <a
             href="#"
@@ -175,17 +170,15 @@
                 d="M5 4 1 8l4 4m10-8 4 4-4 4M11 1 9 15"
               />
             </svg>
-            Code
+            Connect
           </a>
           <h2
             class="text-gray-900 dark:text-white text-3xl font-extrabold mb-2"
           >
-            Best react libraries around the web
+            Reach Out
           </h2>
           <p class="text-lg font-normal text-gray-500 dark:text-gray-400 mb-4">
-            Static websites are now used to bootstrap lots of websites and are
-            becoming the basis for a variety of tools that even influence both
-            web designers and developers.
+            I would love to connect! Please check out my GitHub to see some of the projects I've done and connect on LinkedIn. 
           </p>
           <a
             href="#"
