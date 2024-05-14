e7bcc80a396922ee49f4b71c83f1a4add51db0f2
Sam Kosman <sam.kosman@gmail.com>
2024-05-14 17:23:51 -0600

    Create initial job card to help with showcasing roles

---
+++ src/components/JobCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <div
+    class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+    <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="" alt="">
+    <div class="flex flex-col justify-between p-4 leading-normal">
+      <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ title }}
+      </h5>
+      <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ timeRange }}</p>
+
+      <li v-for="item in items" class="font-normal text-gray-700 dark:text-gray-400">
+        {{ item.message }}
+      </li>
+
+
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+const title = "Fullstack Software Engineer I Vancouver"
+const timeRange = "Feb 2023 - Feb 2024"
+const tools = ["Java", "Spring", "VueJS", "AWS"]
+
+const items = [{ message: 'Experience 1' }, { message: 'Experience 2' }]
+
+</script>
+
+<style></style>
--- src/views/ExperienceView.vue
+++ src/views/ExperienceView.vue
@@ -2,12 +2,20 @@
   <section class="bg-white dark:bg-gray-900">
     <div class="py-8 px-4 mx-auto max-w-screen-xl text-left lg:py-16">
       <h1
-        class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white"
-      >
+        class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
         My Experience
       </h1>
     </div>
+    <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
+      <ProjectCard></ProjectCard>
+    </div>
   </section>
+
 </template>
+
+<script setup>
+import ProjectCard from '@/components/JobCard.vue';
+
+</script>
 
 <style></style>
